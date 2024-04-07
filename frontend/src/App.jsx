5a32796b8bb19114b0de6161b0ef527daee09985
Jonathan Vizcaino <jevizcaino@unimagdalena.edu.co>
2024-04-06 21:05:35 -0500

    (BackEnd): Lexer con actualizaciones terminado

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Header from './components/Header/Header'
+import MainContent from './components/MainContent/MainContent'
+
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <MainContent/>
+    </>
+  )
+}
+
+export default App
